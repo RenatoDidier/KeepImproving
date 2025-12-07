05a6a33b528436185cdf97cd1bb655886301e000
RenatoDidier <renatodidierf@gmail.com>
2025-12-06 22:03:01 -0300

    feat: Configure pulumi file to create webapp with ACR

--- src/external/private/KeepImproving.API/Program.cs
+++ src/external/private/KeepImproving.API/Program.cs
@@ -1,27 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
 app.MapHealthChecks("/health");
+
+app.MapGet("/", () => "API Running");
 
 app.Run();
